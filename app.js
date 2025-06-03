8d1f5fdb74259d249eaffc50922775bc07225a89
Alan-Pinheiro <alanpinheiro5@hotmail.com>
2025-06-03 20:39:38 -0300

    criaçao das rotas GET e POST para visualizar e receber os a fazeres

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
 const express = require ('express');
 const app = express();
 const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars');
+const nodemon = require('nodemon');
+const Tarefa = require('./models/Tarefa');
+
+// Configurações
+
+    // Handlebars
+
+        app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
+
+    // BodyParser
+
+        app.use(bodyParser.urlencoded({extended: false}));
+        app.use(bodyParser.json());
+
+// Rotas
+
+    // GET
+
+        app.get('/tarefas', (req, res) => {
+            res.render('home')
+        })
+
+    // POST
+    
+        app.post('/tarefas', (req, res) => {
+            Tarefa.create({
+                descricao: req.body.descricaoTarefa
+            })
+            .then(() => {
+                res.redirect('/tarefas')
+            })
+            .catch((erro) => {
+                res.send('Deu erro: ' + erro)
+            });
+        });
+
 
 
 app.listen(8080, () => {
